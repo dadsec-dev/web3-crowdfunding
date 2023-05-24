bcfa98b53aeaad15a205cfa5e28db3f83111f918
dadsec-dev <geecypher41@gmail.com>
2023-05-24 20:27:35 +0100

    new

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract CrowdFunding {
+    
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 targetAmount;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _targetAmount, uint256 _deadline, string memory _image) public returns (uint256){
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(_deadline > block.timestamp, "Should be a date in the future");
+
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.targetAmount = _targetAmount;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign() {
+
+    }
+
+    function getDonators() {
+
+    }
+
+    function getCampaigns() {
+        
+    }
 }
